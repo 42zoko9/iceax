005506c7604061a2236f3c7c8e9787ac3b830a0d
42zoko9 <hexproof1988@gmail.com>
2023-09-09 16:36:26 +0900

    Add&Update: Trailの実装

--- src/iceax/trail.py
+++ src/iceax/trail.py
@@ -1,16 +1,18 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from matplotlib.figure import Figure
 
-from .params import FigureParams
-
 
 class Trail:
-    def __init__(self, series: pd.Series) -> None:
-        self.series = series
+    def __init__(self, df: pd.DataFrame, name: str) -> None:
+        self.name = name
+        try:
+            self.series: pd.Series = df[name]
+        except KeyError:
+            raise KeyError('"{}" is not found.'.format(name))
 
     def stats(self) -> pd.DataFrame:
         '''対象の統計量を出力
@@ -20,71 +22,140 @@ class Trail:
         '''
         pass
 
+    # NOTE: pandasのdescribeとほぼ同一
     def __quantitative_stats(self) -> pd.DataFrame:
         '''量的変数及び時系列の統計量(最小・四分位点・最頻値・最大)を出力
 
         Returns:
             pd.DataFrame: 量的変数の統計量をまとめたデータフレーム
         '''
-        pass
+        if self.series.dtype not in ('int', 'float'):
+            raise TypeError('self.series is not quantitative.')
+        s_quant = self.series.quantile([0.25, 0.5, 0.75])
+        s_quant.index = ['q1', 'q2', 'q3']
+        s_min = self.series.min()
+        s_max = self.series.max()
+        s_range = pd.Series({'min': s_min, 'max': s_max})
+        stats = pd.concat([s_range, s_quant])
+        df = pd.DataFrame(stats)
+        df = df.loc[['min', 'q1', 'q2', 'q3', 'max'], :]
+        df.reset_index(inplace=True)
+        df.columns = ['key', 'value']
+        return df
 
-    def plot(self, kind: str, params: FigureParams) -> Figure:
-        '''対象の可視化
-
-        Args:
-            kind (str): グラフの種別
-            params (FigureParams): グラフの体裁を調整するパラメータ群
-
-        Returns:
-            Figure: 可視化結果
-        '''
-        pass
-
-    def __barplot(self, top: Optional[int] = None) -> Figure:
+    def barplot(self, top: Optional[int] = None, figsize: Tuple[Union[float, int], Union[float, int]] = (16., 9.)) -> Figure:
         '''質的変数の値あたりの出現頻度を棒グラフとして出力
 
         Args:
-            top (Optional[int], optional): 可視化する頻度の下限値
+            top (Optional[int], optional): 可視化する頻度の下限値, デフォルト値はNone
+            figsize (Tuple[Union[float, int], Union[float, int]], optional): 画像サイズ，デフォルト値は(16., 9.)
 
         Returns:
             Figure: 棒グラフ
         '''
-        pass
+        df = self.__freq_cnt(top)
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        x = df['key']
+        y = df['value']
+        ax.bar(x, y)
+        ax.set_xlabel('variable')
+        ax.set_ylabel('frequency')
+        ax.yaxis.set_major_formatter('{x:,.0f}')
+        ax.set_title(self.name)
+        return fig
 
-    def __freq_cnt(self) -> pd.DataFrame:
+    def __freq_cnt(self, top: Optional[Union[float, int]] = None) -> pd.DataFrame:
         '''質的変数の値あたりの出現頻度を計算
+
+        Args:
+            top (Union[float, int], optional): 可視化する頻度の下限値, デフォルト値はNone
 
         Returns:
             pd.DataFrame: 出現頻度で降順でソートしたデータフレーム
         '''
-        pass
+        if self.series.dtype != 'object':
+            raise TypeError('self.series is not qualitative.')
+        if (top is not None) and (type(top) not in (float, int)):
+            raise TypeError('"top" must be float, int or None.')
+        cnt = self.series.value_counts(dropna=False)
+        df = pd.DataFrame(cnt)
+        df.sort_values('count', ascending=False, inplace=True)
+        df.reset_index(inplace=True)
+        df.columns = ['key', 'value']
+        if top is not None:
+            df = df.iloc[:top, :]
+        return df
 
-    def __boxplot(self, outlier: Optional[bool] = True) -> Figure:
+    def boxplot(self, outlier: bool = True, figsize: Tuple[Union[float, int], Union[float, int]] = (16., 9.)) -> Figure:
         '''量的変数の分布を箱ひげ図とjitter plotで出力
 
         Args:
             outlier (bool, optional): _description_. Defaults to True.
+            figsize (Tuple[Union[float, int], Union[float, int]], optional): 画像サイズ，デフォルト値は(16., 9.)
 
         Returns:
             Figure: 箱ひげ図
         '''
-        pass
+        if type(outlier) != bool:
+            raise TypeError('"outlier" must be boolean.')
+        x = self.__quantile()
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        sns.boxplot(x=x, color='steelblue', showfliers=False, ax=ax)
+        if outlier:
+            sns.stripplot(x=self.series, dodge=True, jitter=True, color='black', ax=ax)
+        ax.set_xlabel('value')
+        ax.set_ylabel('variable')
+        ax.xaxis.set_major_formatter('{x:,.0f}')
+        ax.set_title(self.name)
+        return fig
 
     def __quantile(self) -> List[float]:
-        '''箱ひげ図出力用に対象の量的変数の四分位点を出力
+        '''箱ひげ図出力用に対象の量的変数の統計量を出力
 
         Returns:
             List[float]: 四分位点の値が格納されたリスト
         '''
-        pass
+        if self.series.dtype not in ('int', 'float'):
+            raise TypeError('self.series is not quantitative.')
+        q = [0.25, 0.5, 0.75]
+        quant = self.series.quantile(q)
+        s_min = self.series.min()
+        s_max = self.series.max()
+        s_range = abs(quant[0.75] - quant[0.25]) * 1.5
+        min_whis = quant[0.25] - s_range
+        min_whis = min_whis if min_whis > s_min else s_min
+        max_whis = quant[0.75] + s_range
+        max_whis = max_whis if max_whis < s_max else s_max
+        result = [min_whis] + quant.tolist() + [max_whis]
+        return result
 
-    def __histgram(self, width: Optional[Union[float, int]] = None) -> Figure:
+    def histgram(self, bins: Optional[List[Union[float, int]]] = None, figsize: Tuple[Union[float, int], Union[float, int]] = (16., 9.)) -> Figure:
         '''量的変数の値あたりの出現頻度をヒストグラムとして出力
 
         Args:
-            width (Optional[Union[float, int]], optional): ビンの幅
+            bins (List[Union[float, int]], optional): ビンの幅, デフォルト値はNone.
+            figsize (Tuple[Union[float, int], Union[float, int]], optional): 画像サイズ，デフォルト値は(16., 9.)
 
         Returns:
             Figure: ヒストグラム
         '''
-        pass
+        if self.series.dtype not in ('int', 'float'):
+            raise TypeError('self.series is not quantitative.')
+        if type(bins) is not list:
+            raise TypeError('"bins" must be list of real number.')
+        invalid_length = len([b for b in bins if b not in (float, int)])
+        if invalid_length > 0:
+            raise ValueError('"bins" is contained invalid values.')
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        if bins is None:
+            ax.hist(self.series, color='steelblue')
+        else:
+            ax.hist(self.series, bins=bins, color='steelblue')
+        ax.set_xlabel('value')
+        ax.set_ylabel('frequency')
+        ax.xaxis.set_major_formatter('{x:,.0f}')
+        ax.yaxis.set_major_formatter('{x:,.0f}')
+        ax.set_title(self.name)
--- tests/test_trail.py
+++ tests/test_trail.py
@@ -1,58 +1,172 @@
+import glob
+from typing import List
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 import pandas as pd
 import pytest
+import seaborn as sns
+from matplotlib.testing.compare import compare_images
 
 from iceax.trail import Trail
+
+
+@pytest.fixture
+def valid_input_barplot() -> pd.DataFrame:
+    l: List = ['a', 'a', 'a', 'b', 'b', 'c', 'e', 'e', np.NaN]
+    df = pd.DataFrame({'tmp': l})
+    return df
+
+
+@pytest.fixture
+def valid_output_barplot() -> str:
+    valid_fn = 'valid.png'
+    x = ['a', 'b', 'e']
+    y = [3, 2, 2]
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot(111)
+    ax.bar(x, y)
+    ax.set_xlabel('variable')
+    ax.set_ylabel('frequency')
+    ax.yaxis.set_major_formatter('{x:,.0f}')
+    ax.set_title('tmp')
+    fig.savefig(valid_fn)
+    yield valid_fn
+    for fn in glob.glob('*.png'):
+        os.remove(fn)
+
+
+@pytest.fixture
+def valid_input_boxplot() -> pd.DataFrame:
+    l: List = [0, 0, 0, 2, 3, 7, 7, 8, 11]
+    df = pd.DataFrame({'tmp': l})
+    return df
+
+
+@pytest.fixture
+def valid_output_boxplot() -> str:
+    valid_fn = 'valid.png'
+    box_x = [0., 0., 3., 7., 11.]
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot(111)
+    sns.boxplot(x=box_x, color='steelblue', showfliers=False, ax=ax)
+    ax.set_xlabel('value')
+    ax.set_ylabel('variable')
+    ax.xaxis.set_major_formatter('{x:,.0f}')
+    ax.set_title('tmp')
+    fig.savefig(valid_fn)
+    yield valid_fn
+    for fn in glob.glob('*.png'):
+        os.remove(fn)
 
 
 class TestQuantitativeStats:
     def test_invalid_not_quantitative(self) -> None:
         '''異常系: 指定したカラムが量的変数ではない
         '''
-        pass
+        invalid_df = pd.DataFrame({'tmp': ['a', 'b']})
+        t = Trail(invalid_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t._Trail__quantitative_stats()
+        err_msg = e.value.args[0]
+        assert err_msg == 'self.series is not quantitative.'
 
     def test_valid(self) -> None:
         '''正常系: 想定通りに出力される
         '''
-        pass
+        valid_result = pd.DataFrame({
+            'key': ['min', 'q1', 'q2', 'q3', 'max'],
+            'value': [0., 0., 3., 7., 11.]
+        })
+        valid_df = pd.DataFrame({'tmp': [0, 0, 0, 2, 3, 7, 7, 8, 11]})
+        t = Trail(valid_df, 'tmp')
+        result = t._Trail__quantitative_stats()
+        pd.testing.assert_frame_equal(valid_result, result)
 
 
 class TestFreqCnt:
     def test_invalid_not_qualitative(self) -> None:
         '''異常系: 指定したカラムが質的変数ではない
         '''
-        pass
+        invalid_df = pd.DataFrame({'tmp': [0, 1, 3]})
+        t = Trail(invalid_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t._Trail__freq_cnt()
+        err_msg = e.value.args[0]
+        assert err_msg == 'self.series is not qualitative.'
+
+    def test_invalid_top(self) -> None:
+        '''異常系: 引数topがNaN及び自然数以外の値を取る
+        '''
+        invalid_top = 'x'
+        input_df = pd.DataFrame({'tmp': ['a', 'a', 'b']})
+        t = Trail(input_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t._Trail__freq_cnt(invalid_top)
+        err_msg = e.value.args[0]
+        assert err_msg == '"top" must be float, int or None.'
 
     def test_valid(self) -> None:
         '''正常系: 想定通りに出力される
         '''
-        pass
+        valid_top = 3
+        valid_result = pd.DataFrame({
+            'key': ['a', 'b', 'e'],
+            'value': [3, 2, 2]
+        })
+        input_df = pd.DataFrame({'tmp': ['a', 'a', 'a', 'b', 'b', 'c', 'e', 'e', np.NaN]})
+        t = Trail(input_df, 'tmp')
+        result = t._Trail__freq_cnt(valid_top)
+        pd.testing.assert_frame_equal(valid_result, result)
 
 
 class TestBarplot:
+    @pytest.mark.skip('TestFreqCnt::test_invalid_not_qualitativeと重複しているためスキップ')
     def test_invalid_not_qualitative(self) -> None:
         '''異常系: 指定したカラムが質的変数ではない
         '''
         pass
 
+    @pytest.mark.skip('TestFreqCnt::test_invalid_topと重複しているためスキップ')
     def test_invalid_top(self) -> None:
         '''異常系: 引数topがNaN及び自然数以外の値を取る
         '''
         pass
+
+    def test_valid(self, valid_input_barplot, valid_output_barplot) -> None:
+        '''正常系: 想定通りに出力される
+        '''
+        result_fn = 'result.png'
+        t = Trail(valid_input_barplot, 'tmp')
+        fig = t.barplot(top=3)
+        fig.savefig(result_fn)
+        result = compare_images(valid_output_barplot, result_fn, 0.001)
+        assert result is None
 
 
 class TestQuantile:
     def test_invalid_not_quantitative(self) -> None:
         '''異常系: 指定したカラムが量的変数ではない
         '''
-        pass
+        invalid_df = pd.DataFrame({'tmp': [True, True, False]})
+        t = Trail(invalid_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t._Trail__quantile()
+        err_msg = e.value.args[0]
+        assert err_msg == 'self.series is not quantitative.'
 
-    def test_valid(self) -> None:
+    def test_valid(self, valid_input_boxplot) -> None:
         '''正常系: 想定通りに出力される
         '''
-        pass
+        valid_result = [0., 0., 3., 7., 11.]
+        t = Trail(valid_input_boxplot, 'tmp')
+        result = t._Trail__quantile()
+        assert result == valid_result
 
 
 class TestBoxplot:
+    @pytest.mark.skip('TestQuantile::test_invalid_not_qualitativeと重複しているためスキップ')
     def test_invalid_not_quantitative(self) -> None:
         '''異常系: 指定したカラムが量的変数ではない
         '''
@@ -61,16 +175,62 @@ class TestBoxplot:
     def test_invalid_outlier(self) -> None:
         '''異常系: 引数outlierにbool値以外の値を取る
         '''
-        pass
+        invalid_outlier = 10
+        input_df = pd.DataFrame({'tmp': [1, 1, 3, 3, 3, 2]})
+        t = Trail(input_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t.boxplot(invalid_outlier)
+        err_msg = e.value.args[0]
+        assert err_msg == '"outlier" must be boolean.'
+
+    # NOTE: jitter-plotは乱数によって都度プロットされる点の位置が異なるためテスト対象から省略
+    def test_valid(self, valid_input_boxplot, valid_output_boxplot) -> None:
+        '''正常系: 想定通りに出力される
+        '''
+        result_fn = 'result.png'
+        valid_outlier = False
+        t = Trail(valid_input_boxplot, 'tmp')
+        fig = t.boxplot(outlier=valid_outlier)
+        fig.savefig(result_fn)
+        result = compare_images(valid_output_boxplot, result_fn, 0.001)
+        assert result is None
 
 
 class TestHistgram:
     def test_invalid_not_quantitative(self) -> None:
         '''異常系: 指定したカラムが量的変数ではない
         '''
-        pass
+        invalid_df = pd.DataFrame({'tmp': ['a', 'b']})
+        t = Trail(invalid_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t.histgram()
+        err_msg = e.value.args[0]
+        assert err_msg == 'self.series is not quantitative.'
 
-    def test_invalid_width(self) -> None:
-        '''正常系: widthがNaNまたは0より大きな実数以外の値を取る
+    def test_invalid_bins_type(self) -> None:
+        '''正常系: binsがNaNまたはリスト以外が与えられている
+        '''
+        input_df = pd.DataFrame({'tmp': [1, 4, 7, 10]})
+        invalid_bins = True
+        t = Trail(input_df, 'tmp')
+        with pytest.raises(TypeError) as e:
+            t.histgram(bins=invalid_bins)
+        err_msg = e.value.args[0]
+        assert err_msg == '"bins" must be list of real number.'
+
+    def test_invalid_bins_values(self) -> None:
+        '''異常系: bins内に実数以外の値が含まれている
+        '''
+        input_df = pd.DataFrame({'tmp': [1, 4, 7, 10]})
+        invalid_bins = [1, 3, 9, True]
+        t = Trail(input_df, 'tmp')
+        with pytest.raises(ValueError) as e:
+            t.histgram(bins=invalid_bins)
+        err_msg = e.value.args[0]
+        assert err_msg == '"bins" is contained invalid values.'
+
+    @pytest.mark.skip('テストの回答データを作成する手順が対象メソッドと同じになるためテストにならない')
+    def test_valid(self) -> None:
+        '''正常系: 想定通りに出力される
         '''
         pass
